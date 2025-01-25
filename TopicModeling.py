b0f60be0f76911db65f07c7fb0aa0108b29f7e94
Akshat K <akshatk1001@gmail.com>
2025-01-25 11:14:19 -0800

    created new matcmaking file

--- TopicModeling.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-df = pd.read_csv("NewScrapedData.csv")
-
-tfidf = TfidfVectorizer(min_df = 5, max_df = .75)
-
-doc_term_matrix = tfidf.fit_transform(df["New Description"])
